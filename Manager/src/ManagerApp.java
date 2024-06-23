2cb5b04fbd30beacfc9834e505399819aa1b9fb5
amishahersavina <amishahersavinasalsabila21@student.cs.unida.gontor.ac.id>
2024-06-24 04:48:14 +0700

    first commit

---
+++ Manager/src/ManagerApp.java
@@ -0,0 +1,12 @@
+ class ManagerApp {
+     public static void main(String[]args)throws Exception{
+
+    
+      var manager1 = new Manager("amisha","google");
+      manager1.sayHello("User1");
+
+     var vicepresident1 = new VicePresident();
+     vicepresident1.sayHello("User1");
+   }
+
+}
